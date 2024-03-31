eb1531bcbc5336a4392e4e675c48fbc1f88d42cd
miha-yy <mojkakec12345@gmail.com>
2024-03-31 04:20:32 +0200

    added create article view

--- views/articles/create.php
+++ views/articles/create.php
@@ -1,3 +1,19 @@
 <div class="container">
     <h3>Objavi novico</h3>
+    <form action="/articles/store" method="POST">
+        <div class="mb-3">
+            <label for="title" class="form-label">Naslov</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="mb-3">
+            <label for="abstract" class="form-label">Abstrakt</label>
+            <input class="form-control" id="abstract" name="abstract">
+        </div>
+        <div class="mb-3">
+            <label for="text" class="form-label">Text</label>
+            <input class="form-control" id="text" name="text">
+        </div>
+        <button type="submit" class="btn btn-primary" name="create">Ustvari</button>
+        <label class="text-danger"><?php echo $error; ?></label>
+    </form>
 </div>
