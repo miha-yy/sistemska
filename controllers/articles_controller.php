d68acd6ba5433bfeb09b7143e1c2bf12094ce642
miha-yy <mojkakec12345@gmail.com>
2024-03-31 04:26:22 +0200

    fixed delete articles function and added list function

--- controllers/articles_controller.php
+++ controllers/articles_controller.php
@@ -20,9 +20,27 @@ class articles_controller
         //s pomočjo statične metode modela, dobimo seznam vseh novic
         //$ads bo na voljo v pogledu za vse oglase index.php
         $articles = Article::all();
-
+        $error = "";
+        $article_error_id = -1;
+        if(isset($_GET["error"])){
+            switch($_GET["error"]){
+                case 1: $error = "Izpolnite vse podatke komentarja"; break;
+                default: $error = "Prišlo je do napake med kreiranjem komentarja.";
+            }
+            $article_error_id=$_GET["article_id"];
+        }
         //pogled bo oblikoval seznam vseh oglasov v html kodo
         require_once('views/articles/index.php');
+    }
+
+    public function list()
+    {
+        //s pomočjo statične metode modela, dobimo seznam vseh novic
+        //$ads bo na voljo v pogledu za vse oglase index.php
+        $articles = Article::my_articles();
+
+        //pogled bo oblikoval seznam vseh oglasov v html kodo
+        require_once('views/articles/list.php');
     }
 
     public function show()
@@ -37,7 +55,29 @@ class articles_controller
         require_once('views/articles/show.php');
     }
 
-    public function create(){
-        require_once('views/articles/create.php');
+    public function delete()
+    {
+        //preverimo, če je uporabnik podal informacijo, o oglasu, ki ga želi pogledati
+        if (!isset($_GET['id'])) {
+            return call('pages', 'error'); //če ne, kličemo akcijo napaka na kontrolerju stran
+            //retun smo nastavil za to, da se izvajanje kode v tej akciji ne nadaljuje
+        }
+        //drugače najdemo oglas in ga prikažemo
+        $article = Article::delete($_GET['id']);
+        header("Location: /articles/list");
     }
+
+    function create(){
+        $error = "";
+        if(isset($_GET["error"])){
+            switch($_GET["error"]){
+                case 1: $error = "Izpolnite vse podatke"; break;
+                default: $error = "Prišlo je do napake med kreiranjem novice.";
+            }
+        }
+        require_once('views/articles/create.php');
+        require_once ('controllers/comments_controller.php');
+    }
+
+
 }
